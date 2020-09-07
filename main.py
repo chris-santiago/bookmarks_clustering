b7712ac0c42645609748536e8737aefb7405103a
cjsantiago <cjsantiago@gatech.edu>
2020-09-07 10:39:00 -0400

    initial work

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans, AffinityPropagation, AgglomerativeClustering
+from sklearn.cluster import KMeans, AffinityPropagation, AgglomerativeClustering, SpectralClustering, DBSCAN
+from sklearn.mixture import GaussianMixture
 from sklearn.decomposition import PCA
 
 input_fn = 'bookmarks_data.p'
@@ -20,15 +21,21 @@ X = pd.DataFrame(
 pca = PCA(n_components=0.999)
 X_pca = pca.fit_transform(X)
 
-mod = AgglomerativeClustering(n_clusters=40)
+mod = AgglomerativeClustering(n_clusters=50)
 mod.fit(X_pca)
+
+# mod = KMeans(n_clusters=50)
+# mod.fit(X_pca)
+
+# mod = SpectralClustering(n_clusters=50)
+# mod.fit(X_pca)
 
 results = data.copy().drop('url_text', axis=1)
 results['label'] = mod.labels_
 results.to_csv('test.csv', index=False)
 
 with open('clusters.txt', 'w') as file:
-    for cluster in range(mod.n_clusters_):
+    for cluster in range(mod.n_clusters):
         file.write(f'Cluster: {cluster}\n')
         file.write('\n'.join(results['title'][results['label'] == cluster].values))
         file.write('\n')
